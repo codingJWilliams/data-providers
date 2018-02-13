1f0bcb9c6070cadf879956949f76f1b835003721
Jay Williams <course@jaywilliams.me>
2018-02-13 19:40:12 +0000

    IT BLOODY FUCKING WORKS

--- 1-base/src/main/java/com/example/android/wearable/complicationsdataprovider/CustomComplicationProviderService.java
+++ 1-base/src/main/java/com/example/android/wearable/complicationsdataprovider/CustomComplicationProviderService.java
@@ -23,11 +23,16 @@ import android.support.wearable.complications.ComplicationData;
 import android.support.wearable.complications.ComplicationManager;
 import android.support.wearable.complications.ComplicationProviderService;
 import android.support.wearable.complications.ComplicationText;
+import android.support.wearable.complications.ProviderUpdateRequester;
 import android.util.Log;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Locale;
 import org.joda.time.DateTime;
+import org.joda.time.DateTimeFieldType;
+import org.joda.time.ReadableInstant;
 
 /**
  * Example watch face complication data provider provides a number that can be incremented on tap.
@@ -48,27 +53,71 @@ public class CustomComplicationProviderService extends ComplicationProviderServi
             int complicationId, int dataType, ComplicationManager complicationManager) {
         Log.d(TAG, "onComplicationActivated(): " + complicationId);
     }
+    public static String roomTimetable[][] = {
+            {"C2-5", "C2-5", "Break", "D2-4", "D2-4", "D1-5", "Lunch", "A0-3"},
+            {"D2-3", "A1-4", "Break", "C2-4", "C2-4", "A0-3", "Lunch", "D2-3"},
+            {"A0-3", "D1-5", "Break", "D2-3", "D2-3", "A1-4", "Lunch", "E1-2"},
+            {"A1-4", "A1-4", "Break", "A2-3", "A2-3", "D1-5", "Lunch", "D1-5"},
+            {"A2-3", "A2-3", "Break", "A1-4", "A1-4", "A0-3", "Lunch", "A0-3"}
+    };
+    public static String timings[] = {
+            "9:15", "10:05", "10:55", "11:10", "12:00", "12:50", "13:40", "14:05", "15:00"
+    };
+    public static int compareTime(String a, String b) {
+        /**
+         * Returns 1 if a is later than B
+         * If a is earlier than b returns -1
+         */
+        String[] aParts = a.split(":");
+        String[] bParts = b.split(":");
 
-    /*
-     * Called when the complication needs updated data from your provider. There are four scenarios
-     * when this will happen:
-     *
-     *   1. An active watch face complication is changed to use this provider
-     *   2. A complication using this provider becomes active
-     *   3. The period of time you specified in the manifest has elapsed (UPDATE_PERIOD_SECONDS)
-     *   4. You triggered an update from your own class via the
-     *       ProviderUpdateRequester.requestUpdate() method.
-     */
+        int aHour = Integer.parseInt(aParts[0]);
+        int aMin = Integer.parseInt(aParts[1]);
+        int bHour = Integer.parseInt(bParts[0]);
+        int bMin = Integer.parseInt(bParts[1]);
+
+        if (aHour < bHour) return -1;
+        if (aHour > bHour) return 1;
+        if (aMin == bMin) return 0;
+        return (aMin >= bMin) ? 1 : -1;
+
+    }
+    public static int whatPeriod(String time) {
+        for (int i = 0; i < timings.length; i++) {
+            if (compareTime(time, timings[i]) == -1) {
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static String getNextPeriodFromTime(String time, int zeroIndexDay) {
+        int period = whatPeriod(time);
+        if (period == -1) return "X";
+        if (period == 8) return "X";
+        String lesson = roomTimetable[zeroIndexDay][period];
+        return lesson;
+    }
+    @SuppressWarnings("deprecation")
+    public static String getTheString() {
+        Date now = new Date();
+        int hrs = now.getHours() + 1;
+        int mins = now.getMinutes() + 1;
+        int day = now.getDay() - 1;
+        if (day == -1 || day == 5) return "X"; // If it's a weekend return X
+        if (hrs >= 15) return "X";
+        if (hrs < 8) return "X";
+        if ((hrs == 8) && (mins < 30)) return "X";
+        return getNextPeriodFromTime(hrs + ":" + mins, day);
+    }
+
     @Override
     public void onComplicationUpdate(
             int complicationId, int dataType, ComplicationManager complicationManager) {
-        Log.d(TAG, "onComplicationUpdate() id: " + complicationId);
-        // Used to create a unique key to use with SharedPreferences for this complication.
-        Date rn = new Date();
-        DateTime rnDt = new DateTime(rn);
-        Integer thing = rnDt.getSecondOfMinute();
-        String numberText = thing.toString();
 
+        Log.d(TAG, "onComplicationUpdate() id: " + complicationId);
+
+        String numberText = getTheString();
+        //int period = getPeriod();
         ComplicationData complicationData = null;
 
         switch (dataType) {
@@ -76,7 +125,7 @@ public class CustomComplicationProviderService extends ComplicationProviderServi
                 complicationData =
                         new ComplicationData.Builder(ComplicationData.TYPE_SHORT_TEXT)
                                 .setShortText(ComplicationText.plainText(numberText))
-                                .setShortTitle(ComplicationText.plainText("Room"))
+                                .setShortTitle(ComplicationText.plainText("Next"))
                                 .build();
                 break;
             default:
