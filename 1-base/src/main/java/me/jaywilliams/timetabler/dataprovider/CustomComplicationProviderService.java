a3ca29185e8b7cb2f6b09d8bdf171591430dc964
Jay Williams <course@jaywilliams.me>
2018-02-14 18:05:11 +0000

    Change stuff

--- 1-base/src/main/java/me/jaywilliams/timetabler/dataprovider/CustomComplicationProviderService.java
+++ 1-base/src/main/java/me/jaywilliams/timetabler/dataprovider/CustomComplicationProviderService.java
@@ -1,42 +1,13 @@
-/*
- * Copyright (C) 2017 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+package me.jaywilliams.timetabler.dataprovider;
 
-package com.example.android.wearable.complicationsdataprovider;
-
-import android.app.PendingIntent;
-import android.content.ComponentName;
-import android.content.SharedPreferences;
 import android.support.wearable.complications.ComplicationData;
 import android.support.wearable.complications.ComplicationManager;
 import android.support.wearable.complications.ComplicationProviderService;
 import android.support.wearable.complications.ComplicationText;
-import android.support.wearable.complications.ProviderUpdateRequester;
 import android.util.Log;
 
-import java.util.Calendar;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.Locale;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeFieldType;
-import org.joda.time.ReadableInstant;
 
-/**
- * Example watch face complication data provider provides a number that can be incremented on tap.
- */
 public class CustomComplicationProviderService extends ComplicationProviderService {
 
     private static final String TAG = "ComplicationProvider";
